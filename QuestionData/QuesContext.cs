94631b64b32360f048fc2600226922b64256bd1e
srashti <ext.srashti.sharma@sncf.fr>
2023-08-13 14:10:48 +0200

    Add the migration and created the database using EF

--- QuestionData/QuesContext.cs
+++ QuestionData/QuesContext.cs
@@ -3,9 +3,11 @@ using QuestionDomain;
 
 namespace QuestionData
 {
-    public class QuesContext :DbContext
+    public class QuesContext : DbContext
     {
         public DbSet<Question> Questions { get; set; }
         public DbSet<AnswerOption> AnswerOptions { get; set; }
+        public QuesContext(DbContextOptions<QuesContext> options) : base(options) { }
+
     }
 }
